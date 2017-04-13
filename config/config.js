cc2a064c774aee5def415d7833e82d0d21d4e64a
D.Xuffy <d.xuffy@qq.com>
2017-04-13 17:59:50 +0800

    d.xuffy update
    d.xuffy update

--- config/config.js
+++ config/config.js
@@ -9,73 +9,91 @@ window.ALL_ROLE = {
     name: '化身幽灵',
     type: 'DOPPELGANGER',
     sequence: 1,
-    maxNum: 1
+    maxNum: 1,
+    image: __uri('/img/DOPPELGANGER.png')
   },
   WEREWOLF: {
     name: '狼人',
     type: 'WEREWOLF',
     sequence: 2,
     assignment: {'n6': 2},
-    maxNum: 2
+    maxNum: 2,
+    image: __uri('/img/WEREWOLF.png')
   },
   MINION: {
     name: '爪牙',
     type: 'MINION',
     sequence: 3,
-    maxNum: 1
+    maxNum: 1,
+    image: __uri('/img/MINION.png')
   },
   MASON: {
     name: '守夜人',
     type: 'MASON',
     sequence: 4,
     assignment: {'n6': 2},
-    maxNum: 2
+    maxNum: 2,
+    image: __uri('/img/MASON.png')
   },
   SEER: {
     name: '预言家',
     type: 'SEER',
     sequence: 5,
     assignment: {'n6': 1},
-    maxNum: 1
+    maxNum: 1,
+    image: __uri('/img/SEER.png')
   },
   ROBBER: {
     name: '强盗',
     type: 'ROBBER',
     sequence: 6,
     assignment: {'n6': 1},
-    maxNum: 1
+    maxNum: 1,
+    image: __uri('/img/ROBBER.png')
   },
   TROUBLEMAKER: {
     name: '捣蛋鬼',
     type: 'TROUBLEMAKER',
     sequence: 7,
     assignment: {'n6': 1},
-    maxNum: 1
+    maxNum: 1,
+    image: __uri('/img/TROUBLEMAKER.png')
   },
   DRUNK: {
     name: '酒鬼',
     type: 'DRUNK',
     sequence: 8,
-    maxNum: 1
+    maxNum: 1,
+    image: __uri('/img/DRUNK.png')
   },
   INSOMNIAC: {
     name: '失眠者',
     type: 'INSOMNIAC',
     sequence: 9,
-    maxNum: 1
+    maxNum: 1,
+    image: __uri('/img/INSOMNIAC.png')
   },
   HUNTER: {
     name: '猎人',
     type: 'HUNTER',
     sequence: 0,
-    maxNum: 1
+    maxNum: 1,
+    image: __uri('/img/HUNTER.png')
   },
   VILLAGER: {
     name: '村民',
     type: 'VILLAGER',
     sequence: 0,
     assignment: {'n6': 2},
-    maxNum: 3
+    maxNum: 3,
+    image: __uri('/img/VILLAGER.png')
+  },
+  TANNER: {
+    name: '皮匠',
+    type: 'TANNER',
+    sequence: 0,
+    maxNum: 1,
+    image: __uri('/img/TANNER.png')
   }
 
 };
--- js/index.js
+++ js/index.js
@@ -11,57 +11,22 @@
     , cardSize
     , numFlag = 0
     , updatePage
+    , GAME_ROLE = []
     , GAME_DATA = {};
 
 
   NUM_KEY = 'n' + PEOPLE_NUM;
+  GAME_DATA.step = GAME_DATA.step || 'game-select';
 
   init();
 
   function init() {
-    var sn, ln, data = [];
 
     // 禁止屏幕滑动
     document.body.addEventListener('touchmove', function (event) {
       event.preventDefault();
     }, false);
 
-    shortNum = $.device.w > $.device.h ? $.device.h : $.device.w;
-    longNum = $.device.w > $.device.h ? $.device.w : $.device.h;
-    sn = (shortNum - ((WIDTH_NUM + 1) * 10)) / WIDTH_NUM;
-    ln = (longNum - ((WIDTH_NUM + 1) * 10)) / HEIGHT_NUM;
-    cardSize = parseInt(sn > ln ? ln : sn);
-
-    $('.card-item').width(cardSize).height(cardSize);
-
-    // 匹配人数角色
-    _.map(ALL_ROLE, function (val) {
-      if (!_.isEmpty(val.assignment) && val.assignment[NUM_KEY]) {
-        for (var i = 0; i < val.assignment[NUM_KEY]; i++) {
-          roleList.push(val);
-        }
-      }
-    });
-
-    // 随机打乱角色
-    roleList = _.shuffle(roleList);
-    for (var i = 1; i <= HEIGHT_NUM * WIDTH_NUM; i++) {
-      var m = $.device.w > $.device.h ? ROLE_SEAT[NUM_KEY].w : ROLE_SEAT[NUM_KEY].h;
-      if (m.indexOf(',' + i + ',') > -1) {
-        data.push({});
-      } else {
-        data.push(roleList[0]);
-        roleList = _.rest(roleList);
-      }
-    }
-    roleList = data;
-
-    // 游戏当前进度 game-select 选择角色、game-distribution 分配角色阶段、game-start 游戏开始
-    GAME_DATA.step = 'game-select';
-    GAME_DATA.roleList = roleList; // 桌上角色位置
-    GAME_DATA.cardSize = cardSize; // 卡牌尺寸 宽=高
-    GAME_DATA.lineNum = $.device.w > $.device.h ? HEIGHT_NUM : WIDTH_NUM; // 横向显示卡牌数量
-    GAME_DATA.screenType = $.device.w > $.device.h ? 'transverse' : 'vertical'; // 屏幕状态：transverse横屏、vertical竖屏
   }
 
   new Vue({
@@ -69,18 +34,27 @@
     created: function () {
       switch (GAME_DATA.step) {
         case 'game-select':
+          $('#select-card-box').removeClass('onw-hide');
           this.gameSelect();
           break;
       }
     },
-    data: {game: GAME_DATA, updatePage: 0},
+    data: {game: GAME_DATA, updatePage: 0, playerNum: 0},
     methods: {
+      /**
+       * 点击卡牌事件
+       * @param e
+       * @param item
+       */
       clickCard: function (e, item) {
         var ele = $(e.currentTarget);
         // 判断游戏进度
         switch (GAME_DATA.step) {
           case 'game-select':
-            item.active = true;
+            var playerNum = 0;
+            item.active = !item.active;
+            playerNum = _.where(this.$data.game.roleList, {active: true}).length;
+            this.$data.playerNum = playerNum > 5 ? playerNum - 3 : 0;
             break;
           case 'game-distribution':
             this.gameDistribution(ele);
@@ -88,20 +62,83 @@
         }
         this.$data.updatePage = Math.random();
       },
+      /**
+       * 选择角色
+       * 注：玩家人数必须是3~12人
+       */
+      clickStartGame: function () {
+        if (!_.findWhere(this.$data.game.roleList, {type: 'WEREWOLF', active: true})) {
+          alert('未选择狼人');
+        } else if (this.$data.playerNum > 12) {
+          alert('最多12个玩家');
+        } else {
+          GAME_ROLE = _.where(this.$data.game.roleList, {active: true});
+          $('#select-card-box').addClass('onw-hide');
+          $('#card-box').removeClass('onw-hide');
+        }
+      },
+      /**
+       * // ...
+       */
       gameSelect: function () {
         GAME_DATA.roleList = [];
         _.map(ALL_ROLE, function (val) {
           for (var i = 0; i < val.maxNum; i++) {
-            GAME_DATA.roleList.push(val);
+            GAME_DATA.roleList.push(_.clone(val));
           }
         });
       },
+      /**
+       * 角色分配
+       * @param ele
+       * @returns {boolean}
+       */
       gameDistribution: function (ele) {
         if ((GAME_DATA.screenType === 'transverse' && ',6,7,8,'.indexOf(',' + ele.index() + ',') > -1) ||
           (GAME_DATA.screenType === 'vertical' && ',4,7,10,'.indexOf(',' + ele.index() + ',') > -1)) {
           return false;
         }
         ele.removeClass('game-distribution').css({'z-index': 1});
+      },
+      initGame: function () {
+        var sn, ln, data = [];
+
+        shortNum = $.device.w > $.device.h ? $.device.h : $.device.w;
+        longNum = $.device.w > $.device.h ? $.device.w : $.device.h;
+        sn = (shortNum - ((WIDTH_NUM + 1) * 10)) / WIDTH_NUM;
+        ln = (longNum - ((WIDTH_NUM + 1) * 10)) / HEIGHT_NUM;
+        cardSize = parseInt(sn > ln ? ln : sn);
+
+        $('.card-item').width(cardSize).height(cardSize);
+
+        // 匹配人数角色
+        _.map(ALL_ROLE, function (val) {
+          if (!_.isEmpty(val.assignment) && val.assignment[NUM_KEY]) {
+            for (var i = 0; i < val.assignment[NUM_KEY]; i++) {
+              roleList.push(val);
+            }
+          }
+        });
+
+        // 随机打乱角色
+        roleList = _.shuffle(roleList);
+        for (var i = 1; i <= HEIGHT_NUM * WIDTH_NUM; i++) {
+          var m = $.device.w > $.device.h ? ROLE_SEAT[NUM_KEY].w : ROLE_SEAT[NUM_KEY].h;
+          if (m.indexOf(',' + i + ',') > -1) {
+            data.push({});
+          } else {
+            data.push(roleList[0]);
+            roleList = _.rest(roleList);
+          }
+        }
+        roleList = data;
+
+        // 游戏当前进度 game-select 选择角色、game-distribution 分配角色阶段、game-start 游戏开始
+        GAME_DATA.step = 'game-distribution';
+        GAME_DATA.roleList = roleList; // 桌上角色位置
+        GAME_DATA.cardSize = cardSize; // 卡牌尺寸 宽=高
+        GAME_DATA.lineNum = $.device.w > $.device.h ? HEIGHT_NUM : WIDTH_NUM; // 横向显示卡牌数量
+        GAME_DATA.screenType = $.device.w > $.device.h ? 'transverse' : 'vertical'; // 屏幕状态：transverse横屏、vertical竖屏
       }
     }
   });
